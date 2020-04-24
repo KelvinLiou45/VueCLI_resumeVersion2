c8a1fa8e74fce21cd3000430f3c6759cef26a012
kelvinliou <kelvinliou45@gmail.com>
2020-04-24 17:12:47 +0800

    updata footer

--- src/elements/footer.vue
+++ src/elements/footer.vue
@@ -2,7 +2,37 @@
   <div id="app">
     <footer>
       <div class="footer_inner">
-        d
+        <div class="footer_inner_contact">
+          <div class="footer_inner_contact_titleEN">
+            <p>Contact me</p>
+          </div>
+          <div class="footer_inner_contact_titleTC">
+            <p>聯繫我</p>
+          </div>
+          <div class="footer_inner_contact_emailButton">
+            <input type="button" value="Email">
+          </div>
+          <div class="footer_inner_contact_phone">
+            <p>0975-202-xxx</p>
+          </div>
+        </div>
+        <div class="footer_inner_link">
+          <div class="footer_inner_link_logo">
+            <div class="footer_inner_link_logo_1">
+              <p>KELVIN</p>
+            </div>
+            <div class="footer_inner_link_logo_2">
+              <p>Front</p>
+              <p>End</p>
+              <p>Engineer</p>
+            </div>
+          </div>
+          <div class="footer_inner_link_content">
+              <p>Github: https://github.com/KelvinLiou45</p>
+              <p>Blogger:</p>
+              <p>Linkedin:</p>
+          </div>
+        </div>
       </div>
     </footer>
   </div>
