ff0411f7eeebf6e154aba56ec0b99a92c722816b
kelvinliou <kelvinliou45@gmail.com>
2020-04-30 14:52:35 +0800

    create rwd

--- src/elements/header.vue
+++ src/elements/header.vue
@@ -36,12 +36,56 @@
             </li>
           </ul>
         </nav>
+        <div class="header_bumger" v-on:click='showBumgerList'>&#9776;</div>
+        <div id="header_bumgerList">
+          <nav>
+            <ul>
+              <li>
+                <router-link to="/" exact-active-class="_active" v-on:click.native='closeList'>首頁</router-link>
+              </li>
+              <li>
+                <router-link to="/introduce" exact-active-class="_active" v-on:click.native='closeList'>個人簡介</router-link>
+              </li>
+              <li>
+                <router-link to="/experience" exact-active-class="_active" v-on:click.native='closeList'>學歷及經驗</router-link>
+              </li>
+              <li>
+                <router-link to="/skill" exact-active-class="_active" v-on:click.native='closeList'>技能</router-link>
+              </li>
+              <li>
+                <router-link to="/portfolio" exact-active-class="_active" v-on:click.native='closeList'>作品集</router-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   </div>
 </template>
 
 <script>
+
+export default {
+  methods:{
+    showBumgerList:function(){
+      let bumgerList = document.getElementById("header_bumgerList");
+      // alert(bumgerList.style.height);
+      if(bumgerList.style.height != '0vh'){
+        bumgerList.style.opacity = "0";
+        bumgerList.style.height = "0vh";
+      }
+      else if(bumgerList.style.height == '0vh'){
+        bumgerList.style.opacity = "1";
+        bumgerList.style.height = "calc(100% - 80px)";
+      }
+    },
+    closeList:function(){
+      let bumgerList = document.getElementById("header_bumgerList");
+      bumgerList.style.opacity = "0";
+      bumgerList.style.height = "0vh";
+    }
+  }
+}
 </script>
 
 <style lang="scss">
